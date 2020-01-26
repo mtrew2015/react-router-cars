0e8af9519f09d6da4229499cc51254e020d537e2
Michael Trew <mtrew2015@gmail.com>
2020-01-25 23:45:41 -0500

    style car component

--- router-cars/src/Components/Car/Car.js
+++ router-cars/src/Components/Car/Car.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Car.scss';
 import { useParams } from 'react-router-dom';
+import './Car.scss';
 
 function Car({ cars, setCars }) {
-    
-    const { id } = useParams();
+	const { id } = useParams();
 	const [ car, setCar ] = useState({});
 
 	useEffect(() => {
@@ -12,18 +12,21 @@ function Car({ cars, setCars }) {
 			return car.id == id;
 		});
 		setCar(actualCar[0]);
-    }, []);
-    
-    console.log(car, 'car')
+	}, []);
 
-    if( !car){
-        return (<h1>OH No That Car is Sold!</h1>)
-    }
+	console.log(car, 'car');
+
+	if (!car) {
+		return <h1>OH No That Car is Sold!</h1>;
+	}
 	return (
-		<div className='card'>
-			<h1>{car.make}</h1>
-			<p>{car.model}</p>
-            <p>${car.price}</p>
+		<div className='car-container'>
+			<div className='card'>
+				<h1>{car.make}</h1>
+				<p>{car.model}</p>
+				<p>${car.price}</p>
+				<p>Call for more details</p>
+			</div>
 		</div>
 	);
 }
